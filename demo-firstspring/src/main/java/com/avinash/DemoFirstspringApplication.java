8edb1a08611dfee413cc7939d4600a5a5b68852c
hp <hp@LAPTOP-82T3M8DC>
2024-07-02 09:04:34 +0530

    commit

--- demo-firstspring/src/main/java/com/avinash/DemoFirstspringApplication.java
+++ demo-firstspring/src/main/java/com/avinash/DemoFirstspringApplication.java
@@ -10,6 +10,7 @@ public class DemoFirstspringApplication {
 		SpringApplication.run(DemoFirstspringApplication.class, args);
 		Student s1=new Student(1,"Avinash","avi@gmail.com","34378268");
 		System.out.println(s1.getRoll()+" "+s1.getName()+" "+s1.getMail()+" "+s1.getMobileno());
+		System.out.println("Hello SpringBoot!");
 	}
 
 }
