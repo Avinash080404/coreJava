6f886080d2c06cc7d1e454dec9f6f678338e222a
hp <hp@LAPTOP-82T3M8DC>
2024-07-02 08:54:15 +0530

    First Commit

---
+++ demo-firstspring/src/main/java/com/avinash/DemoFirstspringApplication.java
@@ -0,0 +1,15 @@
+package com.avinash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFirstspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFirstspringApplication.class, args);
+		Student s1=new Student(1,"Avinash","avi@gmail.com","34378268");
+		System.out.println(s1.getRoll()+" "+s1.getName()+" "+s1.getMail()+" "+s1.getMobileno());
+	}
+
+}
---
+++ demo-firstspring/src/main/java/com/avinash/Student.java
@@ -0,0 +1,18 @@
+package com.avinash;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+	private int roll;
+	private String name;
+	private String mail;
+	private String mobileno;
+	
+	
+
+}
